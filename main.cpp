5a2c1e429fd8bd7781034e7e08927a37b9b69680
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-05-18 19:36:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char a;
+    cin >> a;
+    if (riconoscimento (a)==true){
+        cout << conversione (a);
+    }
+    else {
+        cout << "errore" << endl;
+    }
+
+    return 0;
 }
